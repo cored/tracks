bf3c71eaa0074ec21fa1a764f0808e4889278132
Rafael George <george.rafael@gmail.com>
2013-05-21 17:20:02 -0400

    Refactor tags partial and remove some of the logic from it

--- app/models/stats/tag_cloud.rb
+++ app/models/stats/tag_cloud.rb
@@ -18,6 +18,12 @@ class TagCloud
     @tags
   end
 
+  def relative_size(tag)
+    (tag.count.to_i-min)/divisor
+  end
+
+  private 
+
   def min
     0
   end
@@ -25,8 +31,6 @@ class TagCloud
   def divisor 
     @divisor= ((max - min) / levels) + 1
   end
-
-  private 
 
   def max
     tag_counts.max
