beb6f6063c45cf5b23b0e02d5abda6d96556cb2a
Rafael George <george.rafael@gmail.com>
2013-05-21 16:50:23 -0400

    Refactor: Extract method from TagCloud#compute for tags calculations and put it on the getter

--- app/models/stats/tag_cloud.rb
+++ app/models/stats/tag_cloud.rb
@@ -2,23 +2,25 @@
 # tag cloud code inspired by this article
 #  http://www.juixe.com/techknow/index.php/2006/07/15/acts-as-taggable-tag-cloud/
 class TagCloud
-  attr_reader :user, :tags, :min, :divisor 
+  attr_reader :user, :min, :divisor 
 
   def initialize(user, cut_off = nil)
     @user = user 
     @cut_off = cut_off
   end
 
-  # TODO: parameterize limit
+  def tags
+    unless @tags 
+      params = [sql(@cut_off), user.id]
+      params += [@cut_off, @cut_off] if @cut_off
+      @tags = Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
+    end
+    @tags
+  end
+
   def compute
-    levels=10
-
-    params = [sql(@cut_off), user.id]
-    params += [@cut_off, @cut_off] if @cut_off
-    @tags = Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
-
     max, @min= 0, 0
-    @tags.each { |t|
+    tags.each { |t|
       max = [t.count.to_i, max].max
       @min= [t.count.to_i, @min].min
     }
@@ -31,6 +33,7 @@ class TagCloud
 
   private 
 
+  # TODO: parameterize limit
   def sql(cut_off = nil)
     query = "SELECT tags.id, tags.name AS name, count(*) AS count"
     query << " FROM taggings, tags, todos"
@@ -45,6 +48,10 @@ class TagCloud
     query << " GROUP BY tags.id, tags.name"
     query << " ORDER BY count DESC, name"
     query << " LIMIT 100"
+  end
+
+  def levels
+    10
   end
 
 end
